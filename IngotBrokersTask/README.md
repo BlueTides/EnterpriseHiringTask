2b84ed3cb1cb8ab51bbc95e1cbd6ef149224645d
BlueTides <deltan1ms@live.com>
2024-06-05 14:49:39 +0300

    Committing hiring task

---
+++ IngotBrokersTask/README.md
@@ -0,0 +1,10 @@
+# EnterpriseHiringTask
+
+The following solution is a repond to hiring task at INGOT Financial Brokerage Ltd.
+
+In order for this solution to work correctly user should edit default connection string to SQL databse with a table named Users following this shchema
+
+ - user_id (Primary key - Identity increment 1)
+ - email
+ - password
+ - userName
--- IngotBrokersTask/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ IngotBrokersTask/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a02cd345a092d0352c42fc679effcbceba0b01e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
